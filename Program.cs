de6c2bbc910dd6ede18e8048d620e199a3cc9ff2
Fellmathais <41479603+Fellmathais@users.noreply.github.com>
2019-04-24 23:17:39 -0300

    Exemplo de StringBuilder

---
+++ Program.cs
@@ -0,0 +1,5 @@
+ StringBuilder br = new StringBuilder();
+            br.AppendLine("Bem vindo!"); // Escreve na linha e pula para a proxima
+            br.AppendLine("Ao meu programa");
+
+            Console.WriteLine(br.ToString());
